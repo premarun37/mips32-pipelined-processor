5da4066a63379ddb626ebed52df0343169a6ae3c
Prem Arun P <premarun375@gmail.com>
2026-06-21 22:31:36 +0530

    Added a file

---
+++ sign_extend.v
@@ -0,0 +1,8 @@
+module sign_extend (
+    input  [15:0] imm_in,
+    output [31:0] imm_out
+);
+    // Replicate sign bit (bit 15) into upper 16 bits
+    assign imm_out = {{16{imm_in[15]}}, imm_in};
+endmodule
+
