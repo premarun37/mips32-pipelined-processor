300172ebad8df969cdbab9a6d2a6e87d5c184739
Prem Arun P <premarun375@gmail.com>
2026-06-21 22:30:14 +0530

    Added a file

---
+++ pc.v
@@ -0,0 +1,13 @@
+module pc(
+    input clk,
+    input rst,
+    input [31:0] pc_nxt,
+    output reg [31:0] pc
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            pc <= 32'h00000000;
+        else
+            pc <= pc_nxt;
+    end
+endmodule
